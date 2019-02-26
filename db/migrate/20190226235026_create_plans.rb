13d2568ba69ed7c65d0ce2fe3587b71a973ac35d
Nick Michalski <Nicholas.A.Michalski@gmail.com>
2019-02-26 17:51:48 -0600

    added Plan model

---
+++ db/migrate/20190226235026_create_plans.rb
@@ -0,0 +1,10 @@
+class CreatePlans < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plans do |t|
+      t.string :name
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_26_235026) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
