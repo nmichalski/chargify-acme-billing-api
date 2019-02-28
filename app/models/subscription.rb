807bf9b032a46e5b440c83eab31d659d773cb4c8
Nick Michalski <Nicholas.A.Michalski@gmail.com>
2019-02-27 18:11:17 -0600

    added presence validation on Subscription attributes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,10 @@
 class Subscription < ApplicationRecord
   belongs_to :plan
+
+  validates :plan_id, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
+  validates :shipping_zipcode, presence: true
+  validates :fake_pay_token, presence: true
+  validates :next_billing_date, presence: true
 end
